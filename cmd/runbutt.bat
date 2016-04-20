65b57af77ffe0071b3a4a44ebd5bcd5e594e24ee
Firazenics <Chocolaterulestheworld@yahoo.co.uk>
2016-04-20 17:29:14 +0100

    manface edition
    adasd

---
+++ cmd/runbutt.bat
@@ -0,0 +1,2 @@
+C:\Go\bin\go.exe run "E:\Basty's Stuff\Bots\airhornbot-master\cmd\bot" -r "localhost:6379" -t "MTcyMTA4MjY1MDk2NzQwODY0.Cfg_0A.hb-3fl8WOMwlFyKH02JEQoqnieU" -o "172108265096740864"
+pause
